c34aabf350222bfaadec747cd4a19503ce681805
Denzil Martin <denzil.martin07@gmail.com>
2026-04-18 11:37:29 +1200

    Add project structure and configuration for Aspire application with OpenTelemetry and health checks

---
+++ Aspire/FrontendMentor.InvoiceApp.AspireUtilities/AspireConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace FrontendMentor.InvoiceApp.AspireUtilities;
+
+public static class AspireConstants
+{
+    public static class Databases
+    {
+        public const string AuthDb = "AuthDb";
+    }
+
+    public static class Migrators
+    {
+        public const string IdentityMigrator = "IdentityMigrator";
+    }
+}
