ee98441ee46005b59721094f2fb07e3f1196c786
Denzil Martin <denzil.martin07@gmail.com>
2026-04-18 13:30:28 +1200

    Add Aspire hosting configuration with SQL Server integration and identity migration project reference

--- Aspire/FrontendMentor.InvoiceApp.AppHost/AppHost.cs
+++ Aspire/FrontendMentor.InvoiceApp.AppHost/AppHost.cs
@@ -1,3 +1,18 @@
+using static FrontendMentor.InvoiceApp.AspireUtilities.AspireConstants;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var sqlAdminPassword = builder.AddParameter("SqlAdminPassword", secret: true);
+var sqlServer = builder.AddSqlServer("sql", sqlAdminPassword)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("sql_data");
+
+var authDb = sqlServer.AddDatabase(Databases.AuthDb);
+
+builder.AddProject<Projects.IdentityMigrator>(Migrators.IdentityMigrator)
+    .WithReference(authDb)
+    .WaitFor(authDb)
+    .WithParentRelationship(sqlServer)
+    .ExcludeFromManifest();
 
 builder.Build().Run();
