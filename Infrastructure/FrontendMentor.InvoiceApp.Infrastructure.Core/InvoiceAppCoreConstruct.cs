2941249f93eaf7533475c99811a929c474f0a6a7
Denzil Martin <denzil.martin07@gmail.com>
2026-04-27 11:16:28 +1200

    Add infrastructure for Identity API with CDK configuration, shared resources, and storage setup

---
+++ Infrastructure/FrontendMentor.InvoiceApp.Infrastructure.Core/InvoiceAppCoreConstruct.cs
@@ -0,0 +1,88 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.ECR;
+using Amazon.CDK.AWS.ECS;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.SSM;
+using Constructs;
+
+namespace FrontendMentor.InvoiceApp.Infrastructure.Core;
+
+public sealed class InvoiceAppCoreConstruct : Construct
+{
+    public InvoiceAppCoreConstruct(Construct scope, string id, AppConfig config)
+        : base(scope, id)
+    {
+        var vpc = new Vpc(this, $"vpc-{config.Environment}", new VpcProps
+        {
+            MaxAzs = 3,
+            NatGateways = 1,
+            SubnetConfiguration =
+            [
+                new SubnetConfiguration
+                {
+                    Name = "public",
+                    SubnetType = SubnetType.PUBLIC
+                },
+                new SubnetConfiguration
+                {
+                    Name = "private",
+                    SubnetType = SubnetType.PRIVATE_WITH_EGRESS
+                },
+                new SubnetConfiguration()
+                {
+                    Name = "isolated",
+                    SubnetType = SubnetType.PRIVATE_ISOLATED
+                }
+            ]
+        });
+
+        var repository = new Repository(this, "invoice-app-identity-api", new RepositoryProps
+        {
+            RepositoryName = "invoice-app-identity-api",
+            ImageTagMutability = TagMutability.MUTABLE,
+            RemovalPolicy = RemovalPolicy.DESTROY
+        });
+
+        var cluster = new Cluster(this, "invoice-app-cluster", new ClusterProps
+        {
+            Vpc = vpc,
+            ClusterName = $"invoice-app-cluster-{config.Environment}"
+        });
+
+        var executionRole = new Role(this, "invoice-app-execution-role", new RoleProps
+        {
+            AssumedBy = new ServicePrincipal("ecs-tasks.amazonaws.com"),
+        });
+
+        executionRole.AddManagedPolicy(
+            ManagedPolicy
+                .FromManagedPolicyArn(
+                    this, "TaskExecutionPolicy", "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"));
+
+        var parameters = new Parameters(config);
+        _ = new StringParameter(this, "VpcIdParameter", new StringParameterProps
+        {
+            ParameterName = parameters.VpcId,
+            StringValue = vpc.VpcId
+        });
+
+        _ = new StringParameter(this, "EcrRepository", new StringParameterProps
+        {
+            ParameterName = parameters.EcrIdentityRepositoryName,
+            StringValue = repository.RepositoryName
+        });
+
+        _ = new StringParameter(this, "Cluster", new StringParameterProps
+        {
+            ParameterName = parameters.ClusterArn,
+            StringValue = cluster.ClusterArn
+        });
+
+        _ = new StringParameter(this, "ExecutionRole", new StringParameterProps
+        {
+            ParameterName = parameters.ExecutionRoleArn,
+            StringValue = executionRole.RoleArn
+        });
+    }
+}
---
+++ Infrastructure/FrontendMentor.InvoiceApp.Infrastructure.Identity/IdentityApi.cs
@@ -0,0 +1,169 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.ECR;
+using Amazon.CDK.AWS.ECS;
+using Amazon.CDK.AWS.ElasticLoadBalancingV2;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.SecretsManager;
+using Constructs;
+using HealthCheck = Amazon.CDK.AWS.ECS.HealthCheck;
+using Protocol = Amazon.CDK.AWS.ECS.Protocol;
+using Secret = Amazon.CDK.AWS.ECS.Secret;
+
+namespace FrontendMentor.InvoiceApp.Infrastructure.Identity;
+
+public sealed class IdentityApi : Construct
+{
+    public IdentityApi(Construct scope, string id, AppConfig config, IdentityApiProps props) :
+        base(scope, id)
+    {
+        var loadBalancerSecurityGroup = new SecurityGroup(this, $"load-balancer-security-group-{config.Environment}",
+            new SecurityGroupProps
+            {
+                Vpc = props.Vpc,
+                SecurityGroupName = $"load-balancer-security-group-{config.Environment}",
+                Description = "Security group for the application load balancer",
+                AllowAllOutbound = true
+            });
+        loadBalancerSecurityGroup.AddIngressRule(Peer.AnyIpv4(), Port.HTTP, "Allow HTTP traffic from anywhere");
+
+        var loadBalancer = new ApplicationLoadBalancer(this, $"load-balancer-{config.Environment}", new ApplicationLoadBalancerProps
+        {
+            Vpc = props.Vpc,
+            LoadBalancerName = $"load-balancer-{config.Environment}",
+            InternetFacing = true,
+            SecurityGroup = loadBalancerSecurityGroup
+        });
+
+        var apiSecurityGroup = new SecurityGroup(this, "invoice-app-identity-api-security-group", new SecurityGroupProps
+        {
+            Vpc = props.Vpc,
+            SecurityGroupName = $"invoice-app-identity-api-security-group-{config.Environment}",
+            Description = "Security group for invoice app identity api",
+            AllowAllOutbound = true
+        });
+        apiSecurityGroup
+            .AddIngressRule(
+                Peer.SecurityGroupId(loadBalancerSecurityGroup.SecurityGroupId), Port.HTTP, "Allows HTTP traffic from the load balancer");
+
+        props.DatabaseSecurityGroup
+            .AddIngressRule(
+                Peer.SecurityGroupId(apiSecurityGroup.SecurityGroupId), Port.Tcp(1433), "Allows Database access from the api security group");
+
+        var taskRole = new Role(this, "invoice-app-identity-api-task-role", new RoleProps
+        {
+            AssumedBy = new ServicePrincipal("ecs-tasks.amazonaws.com"),
+        });
+        props.DatabaseSecret!.GrantRead(taskRole);
+
+        taskRole.AddToPolicy(new PolicyStatement(new PolicyStatementProps
+        {
+            Effect = Effect.ALLOW,
+            Actions =
+            [
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter",
+                "ssm:PutParameter"
+            ],
+            Resources =
+            [
+                Arn.Format(new ArnComponents
+                {
+                    Service = "ssm",
+                    Resource = "parameter",
+                    ResourceName = "IdentityApi/DataProtection/*"
+                }, Stack.Of(this))
+            ]
+        }));
+
+        var taskDefinition = new FargateTaskDefinition(this, "invoice-app-identity-api-task-definition",
+            new FargateTaskDefinitionProps
+            {
+                ExecutionRole = props.ExecutionRole,
+                TaskRole = taskRole
+            });
+        taskDefinition.AddContainer($"invoice-app-identity-api-{config.Environment}", new ContainerDefinitionOptions
+        {
+            Image = ContainerImage.FromEcrRepository(props.IdentityRepository, config.ImageTag),
+            PortMappings =
+            [
+                new PortMapping
+                {
+                    Name = "invoice-app-identity-api",
+                    ContainerPort = 8080,
+                    Protocol = Protocol.TCP
+                }
+            ],
+            ContainerName = $"invoice-app-identity-api-{config.Environment}",
+            Secrets = new Dictionary<string, Secret>
+            {
+                ["DB_SECRET"] = Secret.FromSecretsManager(props.DatabaseSecret!)
+            },
+            Logging = LogDriver.AwsLogs(new AwsLogDriverProps
+            {
+                StreamPrefix = $"invoice-app-identity-api-{config.Environment}",
+                Mode = AwsLogDriverMode.NON_BLOCKING,
+                MaxBufferSize = Size.Mebibytes(25)
+            }),
+            HealthCheck = new HealthCheck
+            {
+                Command = ["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost:8080/healthz || exit 1"],
+                Interval = Duration.Seconds(30),
+                Retries = 10,
+                StartPeriod = Duration.Minutes(1),
+                Timeout = Duration.Seconds(30),
+            }
+        });
+
+        var service = new FargateService(this, "invoice-app-identity-api", new FargateServiceProps
+        {
+            Cluster = props.Cluster,
+            TaskDefinition = taskDefinition,
+            DesiredCount = 1,
+            AssignPublicIp = false,
+            VpcSubnets = new SubnetSelection
+            {
+                Subnets = props.Vpc.PrivateSubnets
+            },
+            MinHealthyPercent = 50,
+            SecurityGroups = [apiSecurityGroup],
+            EnableExecuteCommand = true
+        });
+
+        var targetGroup = new ApplicationTargetGroup(this, "invoice-app-identity-api-target-group",
+            new ApplicationTargetGroupProps
+            {
+                Port = 8080,
+                Targets = [service],
+                HealthCheck = new Amazon.CDK.AWS.ElasticLoadBalancingV2.HealthCheck
+                {
+                    Port = "8080",
+                    Path = "/healthz",
+                    HealthyHttpCodes = "200-299"
+                },
+                Vpc = props.Vpc
+            });
+
+        var listener = loadBalancer.AddListener("invoice-app-listener", new ApplicationListenerProps
+        {
+            Port = 80,
+            Open = true
+        });
+
+        listener.AddTargetGroups("ECS", new AddApplicationTargetGroupsProps
+        {
+            TargetGroups = [targetGroup]
+        });
+    }
+}
+
+public sealed record IdentityApiProps
+{
+    public required IVpc Vpc { get; init; }
+    public required IRepository IdentityRepository { get; init; }
+    public required ISecurityGroup DatabaseSecurityGroup { get; init; }
+    public required ISecret? DatabaseSecret { get; init; }
+    public required IRole ExecutionRole { get; init; }
+    public required ICluster Cluster { get; init; }
+}
