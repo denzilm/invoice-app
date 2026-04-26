6153b2366cdfd14b3f4997cd568ca126a3cbf4d7
denzilm <denzil.martin07@gmail.com>
2026-04-27 11:58:42 +1200

    Add AWS integrations, OpenTelemetry instrumentation, and Dockerize Identity API for production deployment (#7)

--- Identity/src/FrontendMentor.InvoiceApp.Identity.Api/Program.cs
+++ Identity/src/FrontendMentor.InvoiceApp.Identity.Api/Program.cs
@@ -1,36 +1,76 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using FrontendMentor.InvoiceApp.Identity.Infrastructure;
 using FrontendMentor.InvoiceApp.Identity.Infrastructure.IdentityPersistence;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.DataProtection.StackExchangeRedis;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using StackExchange.Redis;
 using static FrontendMentor.InvoiceApp.AspireUtilities.AspireConstants;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+var configuration = builder.Configuration;
 
 builder.AddServiceDefaults();
 
+var dbSecretJson = Environment.GetEnvironmentVariable("DB_SECRET");
+if (dbSecretJson is not null)
+{
+    var secret = JsonSerializer.Deserialize<DbSecret>(dbSecretJson)!;
+    configuration[$"ConnectionStrings:{Databases.AuthDb}"] =
+        $"Server={secret.Host},{secret.Port}; Database={Databases.AuthDb};" +
+        $"User Id={secret.Username};Password={secret.Password};TrustServerCertificate=true";
+}
+
 builder.AddSqlServerDbContext<AuthDbContext>(Databases.AuthDb);
-builder.AddRedisClient(Caches.AppCache);
 services.AddProblemDetails();
 services.AddIdentityServices();
-services.AddDataProtection().SetApplicationName(Apis.IdentityApi);
-services.AddSingleton<IConfigureOptions<KeyManagementOptions>>(sp =>
+var dataProtection = services
+    .AddDataProtection()
+    .SetApplicationName(Apis.IdentityApi);
+
+if (builder.Environment.IsProduction())
 {
-    return new ConfigureOptions<KeyManagementOptions>(options =>
+    dataProtection
+        .PersistKeysToAWSSystemsManager($"{Apis.IdentityApi}/DataProtection/Keys");
+}
+else
+{
+    builder.AddRedisClient(Caches.AppCache);
+    services.AddSingleton<IConfigureOptions<KeyManagementOptions>>(sp =>
     {
-        var multiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
-        var database = multiplexer.GetDatabase();
-        options.XmlRepository = new RedisXmlRepository(() => database, $"{Apis.IdentityApi}-DataProtection-Keys");
+        return new ConfigureOptions<KeyManagementOptions>(options =>
+        {
+            var multiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
+            var database = multiplexer.GetDatabase();
+            options.XmlRepository = new RedisXmlRepository(() => database, $"{Apis.IdentityApi}-DataProtection-Keys");
+        });
     });
-});
+}
 
 var app = builder.Build();
 
 app.UseExceptionHandler();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (AuthDbContext context) => $"We have currently '{context.Users.ToList().Count}' users");
+
+app.MapHealthChecks("/healthz");
+
+app.Services.GetRequiredService<AuthDbContext>().Database.Migrate();
 
 app.Run();
+
+internal sealed record DbSecret
+{
+    [JsonPropertyName("host")]
+    public required string Host { get; init; }
+    [JsonPropertyName("port")]
+    public required int Port { get; init; }
+    [JsonPropertyName("username")]
+    public required string Username { get; init; }
+    [JsonPropertyName("password")]
+    public required string Password { get; init; }
+}
